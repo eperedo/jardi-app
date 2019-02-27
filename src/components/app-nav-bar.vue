98dc4ba512b85a814e2eb6d5f4ecdcccd1f4a2f4
Eduardo P. Rivero <jrperedo@gmail.com>
2019-02-27 17:45:40 -0500

    feat(web): Added navbar

---
+++ src/components/app-nav-bar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="navbar-wrapper">
+    <ul class="navbar">
+      <li v-for="item in items" :key="item.id">
+        <nuxt-link :to="item.url">
+          <img v-if="item.icon" :src="item.icon" width="20" height="20">
+          <app-text tag-name="span" :styles="['link-text']" v-if="item.name">{{ item.name }}</app-text>
+        </nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import appText from '@/components/app-text.vue';
+
+export default {
+	name: 'app-nav-bar',
+	components: {
+		appText,
+	},
+	props: {
+		items: {
+			type: Array,
+			default: () => [],
+		},
+	},
+};
+</script>
+
+<style scoped>
+.link-text {
+	color: var(--colorSecondary);
+}
+
+.navbar {
+	align-items: center;
+	display: flex;
+	justify-content: space-between;
+}
+
+.navbar-wrapper {
+	padding: var(--largePadding);
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,45 @@
 <template>
   <section class="wrapper">
-    <app-text tag-name="h1">Home Page</app-text>
+    <app-nav-bar :items="items"/>
   </section>
 </template>
 
 <script>
+import appNavBar from '@/components/app-nav-bar.vue';
 import appText from '@/components/app-text.vue';
+
+function data() {
+	return {
+		items: [
+			{
+				id: 1,
+				icon: '/ham.svg',
+				name: '',
+				url: '/admin/dashboard',
+			},
+			{
+				id: 2,
+				icon: '',
+				url: '/admin/dashboard',
+				name: 'Transactions',
+			},
+			{
+				id: 3,
+				icon: '/search.svg',
+				url: '/admin/search',
+				name: '',
+			},
+		],
+	};
+}
 
 export default {
 	name: 'pages-home',
 	components: {
+		appNavBar,
 		appText,
 	},
+	data,
 };
 </script>
 
@@ -19,7 +47,5 @@ export default {
 .wrapper {
 	background-color: var(--colorPrimary);
 	color: var(--secondaryPrimary);
-	display: grid;
-	justify-content: center;
 }
 </style>
